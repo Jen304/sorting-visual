17a7b164b478a4285c7b90a2c4e37b7ddad60745
Jen <jenhoang304@gmail.com>
2020-12-28 19:56:41 -0800

    update insertion sort

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -17,18 +17,13 @@ const insertionSort = ({ numList, setNewStepList }) => {
     while (j > -1 && current < numArray[j]) {
       numArray[j + 1] = numArray[j];
       j--;
-      //numListStep.push([...numArray]);
       const newColorList = [...defaultColorList];
       newColorList[i] = CUR_POS;
       newColorList[j] = CUR_POS;
-      //colorListStep.push(newColorList);
       addNewStep(numArray, newColorList);
     }
     numArray[j + 1] = current;
   }
-  // numListStep.push([...numArray]);
-  // colorListStep.push(Array(numArray.length).fill(SORTED));
-  //setNewStepList(numListStep, colorListStep);
   addNewStep(numArray, Array(numArray.length).fill(SORTED));
   setStepList();
 };
